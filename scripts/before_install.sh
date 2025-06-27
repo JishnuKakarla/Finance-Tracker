b243e8f3185c44c9280dc838f30675e6f1243325
Jishnu kakarla <86145739+JishnuKakarla@users.noreply.github.com>
2025-06-27 11:51:25 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 echo "Running before_install"
 
 # Remove old frontend files
-rm -rf /home/ubuntu/frontend/*
+sudo rm -rf /home/ubuntu/frontend/*
 
 # Remove old backend JAR
-rm -f /home/ubuntu/backend/app.jar
+sudo rm -f /home/ubuntu/backend/app.jar
