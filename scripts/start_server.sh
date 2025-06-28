4024e43ea9c286d4a178db4be7c14bda23b076f4
Jishnu kakarla <86145739+JishnuKakarla@users.noreply.github.com>
2025-06-28 12:17:54 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 echo "Starting server..."
 
-sudo pkill -f 'serve' || true
-sudo pkill -f 'app.jar' || true
-
+# Start Angular app with PM2
 cd /home/ubuntu/frontend
-nohup serve -s . > /home/ubuntu/frontend/serve.log 2>&1 &
-disown
+pm2 delete serve || true
+pm2 start serve --name serve -- -s . --listen 4200
 
-cd /home/ubuntu/backend
-nohup java -jar app.jar > /home/ubuntu/backend/app.log 2>&1 &
-disown
+# Start Spring Boot app with systemd
+sudo systemctl restart app
 
 echo "Server started."
 exit 0
