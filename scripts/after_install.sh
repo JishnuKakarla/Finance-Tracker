617c6e8b208c3c4c14ddaa8c5b29ea784c8812f4
Jishnu kakarla <86145739+JishnuKakarla@users.noreply.github.com>
2025-06-28 12:17:30 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-
 echo "Running after_install"
 
-# Ensure Node.js and npm are installed
+# Install Node.js and npm if not present
 if ! command -v npm &> /dev/null
 then
-    echo "Installing Node.js and npm..."
     sudo apt-get update
     sudo apt-get install -y nodejs npm
 fi
 
-# Change to your frontend directory
+# Install PM2 globally
+sudo npm install -g pm2
+
+# Install frontend dependencies
 cd /home/ubuntu/frontend
 
 # Install 'serve' globally using npm
 npm install -g serve
 
-# (Optional) Install project dependencies if needed
 npm install
 
 echo "after_install completed"
